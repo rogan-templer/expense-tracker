5748dd218a63126fe989386cc1ceba5f13172f9b
Rogan-Templer <rmtempler@gmail.com>
2021-08-12 10:02:05 +1200

    App.js updated to start importing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
