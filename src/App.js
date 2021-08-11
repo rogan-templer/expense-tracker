5550be36e3dcc47ed1d02da89a92df8583de12a5
Rogan-Templer <rmtempler@gmail.com>
2021-08-12 10:33:42 +1200

    balance, expenses and income components creared and incorporated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
