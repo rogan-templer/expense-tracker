a9501c6de6e73959fc7603862ef7d7a756d85e0b
Rogan-Templer <rmtempler@gmail.com>
2021-08-16 11:01:02 +1200

    IncomeExpense component now calculating correct balance

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function IncomeExpenses() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  /**
+   * Income function below grabs the amount and filters items that are greater than 0 -> then is adds them together (reduce) and gives you a total to 2 decimal places (toFixed)
+   * expense does the same but after filtering any numbers lower than 0
+   */
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   return (
     <>
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">{income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">{expense}</p>
       </div>
     </>
   );
