97f5b94fd94d6311c9bd4594583f0d2eea599312
Rogan-Templer <rmtempler@gmail.com>
2021-08-16 10:47:56 +1200

    transaction component made more dynamic

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,10 +1,20 @@
 import React, { useContext } from "react";
 
 function Transaction({ transaction }) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  /**
+   * Sign definition uses the ternary to determine if the transaction is a positive or negative
+   * Math.abs wrapping transaction amount ensures it is always a whole number then the sign operation will be correct.
+   * Ternary in the class name is dynamic so that the border is red or green depending on the transaction type.
+   */
   return (
     <>
-      <li className="minus">
-        {transaction.text} <span>-$400</span>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
         <button className="delete-btn">x</button>
       </li>
     </>
