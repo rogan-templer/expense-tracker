09f5556a29c0802afde1c2fcf1f88aeffb1c6815
Rogan-Templer <rmtempler@gmail.com>
2021-08-16 10:41:28 +1200

    transaction component created

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+
+function Transaction({ transaction }) {
+  return (
+    <>
+      <li className="minus">
+        {transaction.text} <span>-$400</span>
+        <button className="delete-btn">x</button>
+      </li>
+    </>
+  );
+}
+
+export default Transaction;
