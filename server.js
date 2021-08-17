52208d31ca1eb0655bdb3a5569dbcb71eac42509
Rogan-Templer <rmtempler@gmail.com>
2021-08-18 11:10:34 +1200

    config file set up and server.js fleshed out

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 
+dotenv.config({ path: "./config/config.env" });
+
 const app = express();
 
 app.get("/", (req, res) => res.send("hello"));
 
-app.listen();
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
