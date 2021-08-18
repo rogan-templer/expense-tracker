ff3e04151e421752988fc586122102eb7c4820a5
Rogan-Templer <rmtempler@gmail.com>
2021-08-18 15:42:03 +1200

    post and delete routes set up

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send("GET transactions");
 };
+
+// add a transactions
+// @route POST /api/v1/transactions
+
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+// delete a transactions
+// @route DELETE /api/v1/transactions/:id
+
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE transactions");
+};
