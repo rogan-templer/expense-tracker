76c6929ce27850d47f321ca3dce10d802c05486d
Rogan-Templer <rmtempler@gmail.com>
2021-08-24 15:57:41 +1200

    transaction controller get request modified

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,20 +1,37 @@
+const Transaction = require("../models/Transaction");
+
 // get all transactions
 // @route GET /api/v1/transactions
 
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+//When you use mongoose methods you return a promise and this is why async await is used here.
+
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.send(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
 // add a transactions
 // @route POST /api/v1/transactions
 
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST transactions");
 };
 
 // delete a transactions
 // @route DELETE /api/v1/transactions/:id
 
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE transactions");
 };
