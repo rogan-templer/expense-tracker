4a702a8f576203c828d2a9f9c34c8cd2f768db2a
Rogan-Templer <rmtempler@gmail.com>
2021-08-26 15:39:37 +1200

    morgan added to server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const transactions = require("./routes/transactions");
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json()); //allows use of the body parser on the addTransaction request in transactionController.js
 
 app.use("/api/v1/transactions", transactions);
