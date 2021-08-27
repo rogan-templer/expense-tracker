707f9cdd62f8d2425d1ff75feec818336d473d86
Rogan-Templer <rmtempler@gmail.com>
2021-08-27 14:36:23 +1200

    alteratins made to allow app to be deployed

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
@@ -19,6 +21,16 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json()); //allows use of the body parser on the addTransaction request in transactionController.js
 
 app.use("/api/v1/transactions", transactions);
+
+//path route for the build needs to be entered below the api route as here:
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
